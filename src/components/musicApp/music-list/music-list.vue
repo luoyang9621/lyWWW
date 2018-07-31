ca05e21a329055f7a00247b923e1bd6f538fc96d
luoy <luoy@toolink.cn>
2018-07-31 12:07:53 +0800

    完善music-list页面，开始7-1节课程

--- src/components/musicApp/music-list/music-list.vue
+++ src/components/musicApp/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
     <div class="back">
-        <i class="icon-back"></i>
+        <i class="icon-back" @click='back'></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+        <div class="play-wrapper">
+          <div class="play" v-if="songs.length > 0" ref="playBtn">
+            <i class="icon-play"></i>
+            <span class="text">随机播放全部音乐</span>
+          </div>
+        </div>
         <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer">
@@ -18,14 +24,21 @@
         <div class="song-list-wrapper">
             <song-list :songs="songs"></song-list>
         </div>
+        <div class="loading-container" v-show="!songs.length">
+          <loading></loading>
+        </div>
     </Scroll>
   </div>
 </template>
 <script>
 import songList from 'musicApp/song-list/song-list'
 import Scroll from 'base/scroll'
+import loading from 'base/loading'
+import { prefixStyle } from 'common/js/dom'
 
 const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 export default {
   props: {
@@ -52,27 +65,26 @@ export default {
       let zIndex = 0
       let blur = 0
       let translateY = Math.max(this.minTranslateY, newY)
-      this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-      this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+      this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
       let scale = 1, percent = Math.abs(newY/this.imgHeight)
       if (newY > 0) {
         scale = 1 + percent
       } else {
         blur = Math.min(20 * percent, 20)
       }
-      this.$refs.filters.style['backdrop-filter'] = `blur(${blur}px)`
-      this.$refs.filters.style['-webkitBackdrop-filter'] = `blur(${blur}px)`
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
       if (newY < this.minTranslateY) {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.paddingTop = '70%'
         this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
       }
       this.$refs.bgImage.style.zIndex = zIndex
-      this.$refs.bgImage.style['transform'] = `scale(${scale})`
-      this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
     }
   },
   created () {
@@ -92,10 +104,13 @@ export default {
   methods: {
     scroll (pos) {
       this.scrollY = pos.y
+    },
+    back () {
+      this.$router.back()
     }
   },
   components: {
-    songList, Scroll
+    songList, Scroll, loading
   }
 }
 </script>
